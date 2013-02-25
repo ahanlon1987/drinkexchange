0cc57b68748b4b42854d56dbbf84cd52472bf676
Aaron Hanlon <ahanlon@gmail.com>
2013-02-24 21:10:53 -0600

    dx-ticker front end skeleton

---
+++ source/dx-ticker/config.groovy
@@ -0,0 +1,8 @@
+environments {
+
+    local {
+        warName = 'dx-ticker.war'
+        warDestinationDir =  'C:/dx/tomcat/apache-tomcat-7.0.37/webapps'
+        configLocation = 'C:/dx/config'
+    }
+}
